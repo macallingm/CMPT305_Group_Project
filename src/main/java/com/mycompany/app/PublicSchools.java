2c6106ff86a553bc2180ccee16dd3374520b43fe
theklaw1 <mahar.macalling@gmail.com>
2025-11-15 14:26:37 -0700

    Update PublicSchools.java

--- src/main/java/com/mycompany/app/PublicSchools.java
+++ src/main/java/com/mycompany/app/PublicSchools.java
@@ -108,4 +108,9 @@ public class PublicSchools {
         }
         return typeSchools;
     }
+
+    // method to get all schools
+    public List<PublicSchool> getAllSchools() {
+        return new ArrayList<>(publicSchools);
+    }
 }
